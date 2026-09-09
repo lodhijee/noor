17bbfecee4c50c49ee11abac423d89842d9dc655
lodhijee <mh.mubashir@hotmail.com>
2026-09-09 16:20:01 +0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.noorehidayat.app',
-  appName: 'نور ہدایت',
-  webDir: 'www'
-};
-
-export default config;
